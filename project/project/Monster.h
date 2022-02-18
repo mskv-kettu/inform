e83230d799b3f1f29186bcde772f000e6f1dfbd2
Aleksander <beliai.ar@phystech.edu>
2022-02-18 15:08:00 +0300

    Auto stash before checking out "HEAD"

---
+++ project/project/Monster.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <iostream>
+
+using std::string;
+
+class Monster {
+protected:
+    string Name;
+    unsigned int level;
+    int baseDamage;
+    int CurrentHp;
+public:
+    Monster(string Name, unsigned int level, int baseDamage, int hp);
+
+    virtual ~Monster();
+
+    string getName();
+
+    unsigned int getLevel();
+
+    int getBaseDamage();
+
+    int getCurrentHp();
+
+    void changeHp(int hp);
+};
